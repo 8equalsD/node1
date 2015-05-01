b60b4e23b71f07cea1a44bbd8bf8eb8fc0a5468e
U-Bullkelp\Stoney <rogerharvey888@gmail.com>
2015-05-01 11:45:05 -0700

    working maybe

--- server.js
+++ server.js
@@ -26,22 +26,13 @@ app.post('/', function(req, res) {
 
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running on port:" + app.get('port'))
-})
 
-app.listen(app.post('port'), function() {
-  console.log("Node app is running on port:" + app.get('port'))
-})
+http.listen(app.get('port'), function(){
 
-
-/*
-http.listen(3001, function(){
-
-  console.log('listening on *:3001');
+  console.log('listening on *:5000');
 
 });
-*/
+
 
 /*
 
